4e0e1a52bb0f8e19156c81e6e1733952bdc81312
Syahril0852 <55231177+Syahril0852@users.noreply.github.com>
2020-02-25 08:32:28 +0700

    authentication

--- laravelapisyahril/routes/api.php
+++ laravelapisyahril/routes/api.php
@@ -13,6 +13,20 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
 Route::apiResource('artikel','ArtikelAPIController');
 
 Route::apiResource('berita','BeritaAPIController');
@@ -24,3 +38,9 @@ Route::apiResource('galeri','GaleriAPIController');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*Route::get('soal1','BabSatuController@a1')->name('soal1');
+
+Route::get('soal2','BabSatuController@a2');
+*/
+
